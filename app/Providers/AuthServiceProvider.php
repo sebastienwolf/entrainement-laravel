7f8d1a97604fdd59b58b63be8fdc17ff0dd23095
sebastienwolf <s.mouret.pro@gmail.com>
2022-10-11 10:36:53 +0200

    exe 17 migrate bdd

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,8 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
+        //$this->registerPolicies();
+        Schema::defaultStringLength(191);
         //
     }
 }
